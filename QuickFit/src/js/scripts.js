ea6e22077497c27e221a33f48f1dc06be9e19c16
alricafort <56417303+alricafort@users.noreply.github.com>
2020-03-25 12:16:25 -0400

    Update scripts.js

--- QuickFit/src/js/scripts.js
+++ QuickFit/src/js/scripts.js
@@ -2,3 +2,8 @@ $('.login').on('click', function () {
     window.location.href = '..\\login.html';
     return false;
 });
+
+// make the next button appear
+function makeVisible(){
+  document.getElementById("nav-btn-right").style.visibility = "visible";
+}
